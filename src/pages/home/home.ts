9ebb1d014537d1cda2c18ff42a5ac2a4fbccabec
virendrachy <v.kumar@easternenterprise.com>
2017-05-17 12:27:46 +0530

    add slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,76 +7,76 @@ import {ShareService} from '../services/ShareService'
 import {ENV} from '../../config/environment.prod'
 
 @Component({
-    selector: 'page-home',
-    templateUrl: 'home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-    items: any;
+  items: any;
 
-    constructor(public navCtrl: NavController, public http: Http, private shareService: ShareService) {
-        let month = this.shareService.getMonth();
-        let year = this.shareService.getYear();
+  constructor(public navCtrl: NavController, public http: Http, private shareService: ShareService) {
+    let month = this.shareService.getMonth();
+    let year = this.shareService.getYear();
 
-        if (month && year) {
-            this.http.get(ENV.API_URL + '?year=' + year + '&month=' + month).map(res => res.json()).subscribe(data => {
-                this.items = data;
-            });
-        } else if (year) {
-            this.http.get(ENV.API_URL + '?year=' + year).map(res => res.json()).subscribe(data => {
-                this.items = data;
-            });
-        } else if (month) {
-            this.http.get(ENV.API_URL + '?month=' + month).map(res => res.json()).subscribe(data => {
-                this.items = data;
-            });
-        } else {
-            this.http.get(ENV.API_URL).map(res => res.json()).subscribe(data => {
-                this.items = data;
-            });
-        }
-
+    if (month && year) {
+      this.http.get(ENV.API_URL + '?year=' + year + '&month=' + month).map(res => res.json()).subscribe(data => {
+        this.items = data;
+      });
+    } else if (year) {
+      this.http.get(ENV.API_URL + '?year=' + year).map(res => res.json()).subscribe(data => {
+        this.items = data;
+      });
+    } else if (month) {
+      this.http.get(ENV.API_URL + '?month=' + month).map(res => res.json()).subscribe(data => {
+        this.items = data;
+      });
+    } else {
+      this.http.get(ENV.API_URL).map(res => res.json()).subscribe(data => {
+        this.items = data;
+      });
     }
 
-    doInfinite(infiniteScroll) {
-        let month = this.shareService.getMonth();
-        let year = this.shareService.getYear();
+  }
 
-        setTimeout(() => {
-            if (month && year) {
-                this.http.get(ENV.API_URL + '?year=' + year + '&month=' + month + '&offset=' + this.items.length).map(res => res.json()).subscribe(data => {
-                    for (var i = 0; i < data.length; i++) {
-                        this.items.push(data[i]);
-                    }
-                });
-            } else if (year) {
-                this.http.get(ENV.API_URL + '?year=' + year + '&offset=' + this.items.length).map(res => res.json()).subscribe(data => {
-                    for (var i = 0; i < data.length; i++) {
-                        this.items.push(data[i]);
-                    }
-                });
-            } else if (month) {
-                this.http.get(ENV.API_URL + '?month=' + month + '&offset=' + this.items.length).map(res => res.json()).subscribe(data => {
-                    for (var i = 0; i < data.length; i++) {
-                        this.items.push(data[i]);
-                    }
-                });
-            } else {
-                this.http.get(ENV.API_URL + '?offset=' + this.items.length).map(res => res.json()).subscribe(data => {
-                    for (var i = 0; i < data.length; i++) {
-                        this.items.push(data[i]);
-                    }
-                });
-            }
-            infiniteScroll.complete();
-        }, 500);
-    }
+  doInfinite(infiniteScroll) {
+    let month = this.shareService.getMonth();
+    let year = this.shareService.getYear();
 
-    itemTapped(event, item) {
-
-        this.navCtrl.push(StripPage, {
-            stripTitle: item.title,
-            strip: item.mainImage
+    setTimeout(() => {
+      if (month && year) {
+        this.http.get(ENV.API_URL + '?year=' + year + '&month=' + month + '&offset=' + this.items.length).map(res => res.json()).subscribe(data => {
+          for (var i = 0; i < data.length; i++) {
+            this.items.push(data[i]);
+          }
         });
-    }
+      } else if (year) {
+        this.http.get(ENV.API_URL + '?year=' + year + '&offset=' + this.items.length).map(res => res.json()).subscribe(data => {
+          for (var i = 0; i < data.length; i++) {
+            this.items.push(data[i]);
+          }
+        });
+      } else if (month) {
+        this.http.get(ENV.API_URL + '?month=' + month + '&offset=' + this.items.length).map(res => res.json()).subscribe(data => {
+          for (var i = 0; i < data.length; i++) {
+            this.items.push(data[i]);
+          }
+        });
+      } else {
+        this.http.get(ENV.API_URL + '?offset=' + this.items.length).map(res => res.json()).subscribe(data => {
+          for (var i = 0; i < data.length; i++) {
+            this.items.push(data[i]);
+          }
+        });
+      }
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+  itemTapped(event, item) {
+    this.navCtrl.push(StripPage, {
+      stripTitle: item.title,
+      strip: item.mainImage,
+      items: this.items,
+    });
+  }
 }
--- src/pages/strip/strip.ts
+++ src/pages/strip/strip.ts
@@ -2,17 +2,45 @@ import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 
 @Component({
-    selector: 'page-strip',
-    templateUrl: 'strip.html'
+  selector: 'page-strip',
+  templateUrl: 'strip.html'
 })
 export class StripPage {
-    stripTitle: String;
-    strip: String;
+  stripTitle: String;
+  strip: String;
+  nextTitle: String;
+  prevTitle: String;
+  prevMainImage: String;
+  nextMainImage: String;
+  items: any;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-        // If we navigated to this page, we will have an item available as a nav param
-        this.stripTitle = navParams.get('stripTitle');
-        this.strip = navParams.get('strip');
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.stripTitle = navParams.get('stripTitle');
+    this.strip = navParams.get('strip');
+    this.items = navParams.get('items');
+  }
 
+  previousImage(event) {
+    this.items.forEach((item, index) => {
+      if (item.title == this.stripTitle) {
+        this.prevTitle = (index > 0) ? this.items[index - 1].title : this.items[0].title;
+        this.prevMainImage = (index > 0) ? this.items[index - 1].mainImage : this.items[0].mainImage;
+        this.stripTitle = this.prevTitle;
+        this.strip = this.prevMainImage;
+      }
+    });
+  }
+
+  nextImage(event, title) {
+    let itemLength = this.items.length;
+    this.items.forEach((item, index) => {
+      if (item.title == title) {
+        this.nextTitle = (index < (itemLength - 1)) ? this.items[index + 1].title : this.items[itemLength - 1].title;
+        this.nextMainImage = (index < (itemLength - 1)) ? this.items[index + 1].mainImage : this.items[itemLength - 1].mainImage;
+        this.stripTitle = this.nextTitle;
+        this.strip = this.nextMainImage;
+      }
+    });
+  }
 }
